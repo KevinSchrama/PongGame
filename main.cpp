9fedea0d7bbe922c458cff8c5aa89bb58d2342bf
KevinSchrama <55480855+KevinSchrama@users.noreply.github.com>
2024-05-30 19:10:53 +0200

    Begin adding opencv

--- main.cpp
+++ main.cpp
@@ -2,13 +2,37 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <chrono>
+#include <opencv2/opencv.hpp>
 
 #include "Game/Game.h"
 
-int main() {
-    Game pongGame;
+using namespace cv;
 
-    pongGame.Run();
+int main() {
+//    Game pongGame;
+//
+//    pongGame.Run();
+    Mat image;
+    VideoCapture cap(0);//Declaring an object to capture stream of frames from default camera//
+    if (!cap.isOpened()){ //This section prompt an error message if no video stream is found//
+        std::cout << "No video stream detected" << std::endl;
+        system("pause");
+        return-1;
+    }
+
+    while (true){ //Taking an everlasting loop to show the video//
+        cap >> image;
+        if (image.empty()){ //Breaking the loop if no video frame is detected//
+            break;
+        }
+        imshow("Video Player", image);//Showing the video//
+        char c = (char)waitKey(25);//Allowing 25 milliseconds frame processing time and initiating break condition//
+        if (c == 27){ //If 'Esc' is entered break the loop//
+            break;
+        }
+    }
+    cap.release();
+
 
     return 0;
 }
